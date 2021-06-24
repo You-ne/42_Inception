78b1fff9e4ae5101c570fac44e1ad63dbc60d892
user42 <yotillar@student.42.fr>
2021-06-24 15:52:44 +0200

    push

---
+++ srcs/services/mariadb/start.sh
@@ -0,0 +1,8 @@
+
+# Database SQL (access to root user no pwd)
+service mysql restart
+#echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
+#echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root
+#echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root
+#echo "FLUSH PRIVILEGES;" | mysql -u root
+
---
+++ srcs/services/wordpress/start.sh
@@ -0,0 +1,13 @@
+ Php enable server to give php files
+service php7.3-fpm start
+
+# Wordpress
+#mv wordpress/ /var/www/myServer/
+#mv /tmp/wp-config.php /var/www/myServer/wordpress
+
+# Php my admin
+#mv /tmp/config.inc.php /var/www/myServer/phpmyadmin/config.inc.php
+
+# Allow access to nginx User 
+chown -R www-data:www-data /var/www/* # www-data:www-data == user:group ; www-data == nginx
+chmod -R 755 /var/www/*
